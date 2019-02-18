56c69d49df89ac2b7a18036d616c9af3ef603b2f
lkaniewski <46943424+lkaniewski@users.noreply.github.com>
2019-02-17 21:48:13 -0600

    Add files via upload

--- StepperMotorTry1.ino
+++ StepperMotorTry1.ino
@@ -1,3 +1,4 @@
+#include <Stepper.h>
 #define IN1 2
 #define IN2 3
 #define IN3 4
@@ -10,10 +11,6 @@ void setup() {
   // put your setup code here, to run once:
   motor.setSpeed(60);
 
-  motor.step(Steps); //clockwise
-  delay(500);
-  motor.step(-Steps); //counter clockwise 
-
 }
 //Stepper(steps, pin1, pin2)
 //Stepper(steps, pin1, pin2, pin3, pin4)
@@ -22,6 +19,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  motor.step(Steps); //clockwise
+  delay(500);
+  motor.step(-Steps); //counter clockwise 
 
 }
 //reference: https://www.arduino.cc/en/Tutorial/StepperOneRevolution
