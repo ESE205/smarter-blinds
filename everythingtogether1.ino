0337c46bcfa89cd0d2541cfc4b699ed508f139e0
lkaniewski <46943424+lkaniewski@users.noreply.github.com>
2019-03-06 20:17:31 -0600

    Add files via upload
    As of March 7 - 8:17 pm

--- everythingtogether1.ino
+++ everythingtogether1.ino
@@ -1,18 +1,19 @@
 
 #define aref_voltage 5
 
-const int photoresistor = A0;
-int tempPin = A1;
-int tempReading;
-int value;
+const int photoresistor = 1;
+int temperaturePin = 0;
 int current_steps = 0;
-int steps_needed = 2038;
+int steps_needed = 2038; //update here 
 boolean dir = true;
 int Pin0 = 8;
 int Pin1 = 9;
 int Pin2 = 10;
 int Pin3 = 11;
 int _step = 0;
+int steps = 0;
+int value;
+boolean userInput = false;
 
 void setup() {
  Serial.begin(9600);
@@ -23,38 +24,47 @@ void setup() {
 }
 
 void loop() {
-  tempReading = analogRead(tempPin);
+  float voltage, temperatureC, temperatureF;
+  voltage = getVoltage(temperaturePin);
+  temperatureC = (voltage-0.5)*100.0;
+  temperatureF= temperatureC *(9.0/5.0)+32.0;
   value = analogRead(photoresistor);
 
-  Serial.print("Temp reading: ");
-  Serial.print(tempReading);
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.print("  deg C: ");
+  Serial.print(temperatureC);
+  Serial.print("  deg F: ");
+  Serial.print(temperatureF);
+  Serial.print(" Photoresistor: ");
+  Serial.println(value);
 
-  float voltage = tempReading * aref_voltage;
-  voltage /= 1024.0; 
 
-  float temperatureC = (voltage - 0.5) * 100 ;  
-  Serial.print(temperatureC); 
-  Serial.println(" degrees C");
-  
-  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
-  Serial.print(temperatureF); 
-  Serial.println(" degrees F");
-
-  value = analogRead(photoresistor);
-  Serial.println(value); 
   
   delay(1000);
-
-  if (value < 170) {
-   while(current_steps<=steps_needed){
-   stepperMotorFunction();
-   }
+  if (userInput) {
+    //
+  } else {
+    //Increase Temperature, Open Blinds
+    //Increase Brightness, Open blinds
+  if (value < 180 || temperatureF > 80) {
+    //update steps_needed here 
+    dir = true;
+  stepperMotorFunction();
    current_steps=0; 
+  } 
+    //Decrease Temperature, Close Blinds
+    //Decrease Brightness, Close Blinds
+  if (value > 160 || temperatureF < 70){
+   dir = false;
+   stepperMotorFunction();
   }
 }
 
-void stepperMotorFunction(){
+}
 
+void stepperMotorFunction(){
+for (int i =0; i < steps_needed; i++){
   switch(_step){
  case 0:
  digitalWrite(Pin0, LOW);
@@ -125,5 +135,10 @@ void stepperMotorFunction(){
  _step=7;
  }
  delay(1);
+}
  return current_steps;
+}
+
+float getVoltage(int pin) { 
+  return (analogRead(pin) * (5.0/1024.0));
 }
