fededa28f4f5245cdb231c96ed8c3e5984e8dd32
lkaniewski <46943424+lkaniewski@users.noreply.github.com>
2019-03-06 21:04:53 -0600

    Add files via upload

--- Clockwise_0to90_.ino
+++ Clockwise_0to90_.ino
@@ -1,22 +1,16 @@
-
-#define aref_voltage 5
-
-const int photoresistor = 1;
-int temperaturePin = 0;
-int current_steps = 0;
-int steps_needed = 2038; //update here 
-boolean dir = true;
 int Pin0 = 8;
 int Pin1 = 9;
 int Pin2 = 10;
 int Pin3 = 11;
 int _step = 0;
-int steps = 0;
-int value;
-boolean userInput = false;
+int current_steps = 0;
+int steps_needed = 4076; //44836 = 0 to 90 degrees
+boolean dir = true;
+int total_rev = 11;
+int current_rev=0;
+
 
 void setup() {
- Serial.begin(9600);
  pinMode(Pin0, OUTPUT);
  pinMode(Pin1, OUTPUT);
  pinMode(Pin2, OUTPUT);
@@ -24,45 +18,17 @@ void setup() {
 }
 
 void loop() {
-  float voltage, temperatureC, temperatureF;
-  voltage = getVoltage(temperaturePin);
-  temperatureC = (voltage-0.5)*100.0;
-  temperatureF= temperatureC *(9.0/5.0)+32.0;
-  value = analogRead(photoresistor);
-
-  Serial.print("voltage: ");
-  Serial.print(voltage);
-  Serial.print("  deg C: ");
-  Serial.print(temperatureC);
-  Serial.print("  deg F: ");
-  Serial.print(temperatureF);
-  Serial.print(" Photoresistor: ");
-  Serial.println(value);
-  delay(5000);
-  
-  if (userInput) {
-    //
-  } else {
-    //Increase Temperature, Open Blinds
-    //Increase Brightness, Open blinds
-  if (value < 180 || temperatureF > 80) {
-    //update steps_needed here 
-    dir = true;
-  stepperMotorFunction();
-   current_steps=0; 
-  } 
-    //Decrease Temperature, Close Blinds
-    //Decrease Brightness, Close Blinds
-  if (value > 160 || temperatureF < 70){
-   dir = false;
+ while(current_rev < total_rev){
+  while(current_steps<=steps_needed){
    stepperMotorFunction();
-  }
+  } 
+  current_rev++;
+  current_steps=0;
 }
-
 }
 
 void stepperMotorFunction(){
-for (int i =0; i < steps_needed; i++){
+
   switch(_step){
  case 0:
  digitalWrite(Pin0, LOW);
@@ -133,10 +99,5 @@ for (int i =0; i < steps_needed; i++){
  _step=7;
  }
  delay(1);
-}
  return current_steps;
-}
-
-float getVoltage(int pin) { 
-  return (analogRead(pin) * (5.0/1024.0));
 }
